13ddb9adba229d9df42233141e88c41f1853b3f1
deliahellander <70303141+deliahellander@users.noreply.github.com>
2021-01-13 15:18:16 -0700

    spread out scales to avoid overlap

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -36,11 +36,11 @@ d3.csv("assets/data/data.csv").then(function(data) {
     // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([20, d3.max(data, d => d.income)])
+      .domain([38000, d3.max(data, d => d.income)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(data, d => d.obesity)])
+      .domain([20, d3.max(data, d => d.obesity)])
       .range([height, 0]);
 
     // Step 3: Create axis functions
