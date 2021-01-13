070f60e03ff0bd58e7f7cbf3566c2bc3a5dee8aa
deliahellander <70303141+deliahellander@users.noreply.github.com>
2021-01-13 14:59:09 -0700

    finished scatter plot and HTML edits

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,7 +1,7 @@
 // Used Hair-Metal example from class as a model
 
-var svgWidth = 960;
-var svgHeight = 500;
+var svgWidth = 700;
+var svgHeight = 450;
 
 var margin = {
   top: 20,
@@ -14,7 +14,7 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".chart")
+var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -23,7 +23,7 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("data.csv").then(function(data) {
+d3.csv("assets/data/data.csv").then(function(data) {
 
     // Step 1: Parse Data/Cast as numbers
     // Incomve vs. Obesity
@@ -66,7 +66,63 @@ d3.csv("data.csv").then(function(data) {
     .attr("cx", d => xLinearScale(d.income))
     .attr("cy", d => yLinearScale(d.obesity))
     .attr("r", "15")
-    .attr("fill", "blue")
+    .attr("fill", "teal")
     .attr("opacity", ".75");
 
-    
+    // Step 6: Initialize tool tip
+    // ==============================
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -60])
+      .html(function(d) {
+        return (`${d.state}<br>Income: ${d.income}<br>Obesity: ${d.obesity}`);
+      });
+
+    // Step 7: Create tooltip in the chart
+    // ==============================
+    chartGroup.call(toolTip);
+
+    // Step 8: Create event listeners to display and hide the tooltip
+    // ==============================
+    circlesGroup.on("click", function(data) {
+      toolTip.show(data, this);
+    })
+      // onmouseout event
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
+
+    // Create axes labels
+    chartGroup.append("text")
+      .style("font-size", "11px")
+      .selectAll("tspan")
+      .data(data)
+      .enter()
+      .append("tspan")
+        .attr("x", function(data) {
+            return xLinearScale(data.income -20);
+          })
+          .attr("y", function(data) {
+            return yLinearScale(data.obesity);
+          })
+          .text(function(data) {
+            return data.abbr
+          });
+
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Obesity(%)");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("Income($)");
+  }).catch(function(error) {
+    console.log(error);
+
+
+  });
